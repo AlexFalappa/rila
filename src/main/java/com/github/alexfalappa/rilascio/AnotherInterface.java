a0a8ba17737a57487ee0066729576f800fec4138
Alessandro Falappa <alex.falappa@gmail.com>
2019-02-22 16:08:46 +0100

    Expansion of API

---
+++ src/main/java/com/github/alexfalappa/rilascio/AnotherInterface.java
@@ -0,0 +1,13 @@
+package com.github.alexfalappa.rilascio;
+
+/**
+ * MyLib expansion.
+ *
+ * @author Alessandro Falappa
+ */
+public interface AnotherInterface {
+
+    float aValue();
+
+    int yetAnotherValue();
+}
---
+++ src/main/java/com/github/alexfalappa/rilascio/MyLibCalculator.java
@@ -0,0 +1,20 @@
+package com.github.alexfalappa.rilascio;
+
+/**
+ * Implementation of AnotherInterface.
+ *
+ * @author Alessandro Falappa
+ */
+public class MyLibCalculator implements AnotherInterface {
+
+    @Override
+    public float aValue() {
+        return 3.1415f;
+    }
+
+    @Override
+    public int yetAnotherValue() {
+        return 42;
+    }
+
+}
